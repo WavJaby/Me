81bf2fa7ada22349f1a86f1e89865fc679515ff8
WavJaby <WavJaby@gmail.com>
2021-10-20 08:19:05 +0800

    just edit

--- window.js
+++ window.js
@@ -44,6 +44,11 @@ function Window() {
 		) + 'px';
 	}
 	
+	// 數值
+	this.getBodyHeight = function() {
+		return windowBody.offsetHeight;
+	}
+	
 	// 功能
 	this.setTitle = function(text) {
 		windowTitle.innerText = text;
@@ -63,8 +68,94 @@ function Window() {
 		setWindowBodyHeight();
 	}
 	
-	// 數值
-	this.getBodyHeight = function() {
-		return windowBody.offsetHeight;
+}
+
+function MinimizeWindow(canvasElement) {
+	let ctx = canvasElement.getContext("2d");
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	canvasElement.style.display = 'none';
+	
+	const eles = [];
+	this.addElement = function(ele) {
+		const itemData = {};
+		itemData.bound = ele.getBoundingClientRect();
+		if (ele.tagName == 'IMG') {
+			itemData.tagName = 'img';
+			itemData.image = ele; 
+		} else if (ele.tagName == 'P') {
+			itemData.tagName = 'p';
+			itemData.fillStyle = ele.getStyle('color');
+			itemData.font = ele.getStyle('font');
+			itemData.text = ele.innerText;
+			itemData.height = parseInt(ele.getStyle('font-size')) * 1.1;
+		} else if (ele.tagName == 'DIV') {
+			itemData.tagName = 'div';
+			itemData.fillStyle = ele.getStyle('background-color');
+		}
+		eles.push(itemData);
+	}
+	
+	this.setFakeText = function(height, eleBound) {
+		const fillStyle = 'rgb(200,200,200)';
+		const bound = eleBound;
+		const gap = 5;
+		const itemHeight = 25;
+		const width = [];
+		for (let i = gap; i < height; i += itemHeight + gap)
+			width.push(Math.random() * bound.width / 3 + itemHeight);
+		
+		const itemData = {};
+		itemData.draw = function(canvas) {
+			for (let i = 0; i < width.length; i++) 
+				canvas.fillRoundRect(bound.left, bound.top + i * (itemHeight + gap) + gap, width[i] , itemHeight, itemHeight / 2);
+		}
+		itemData.fillStyle = fillStyle;
+		eles.push(itemData);
+	}
+	
+	function drawElements(canvas) {
+		for (let i = 0; i < eles.length; i++){
+			const ele = eles[i];
+			const bound = ele.bound;
+			if (ele.tagName == 'img') {
+				canvas.drawImage(ele.image, bound.left, bound.top, bound.width, bound.height);
+			} else if (ele.tagName == 'p') {
+				canvas.fillStyle = ele.fillStyle;
+				canvas.font = ele.font;
+				canvas.fillText(ele.text, bound.left, bound.top + ele.height);
+			} else if (ele.tagName == 'div') {
+				canvas.fillStyle = ele.fillStyle;
+				canvas.fillRect(bound.left, bound.top, bound.width, bound.height);
+			} else {
+				canvas.fillStyle = ele.fillStyle;
+				ele.draw(canvas);
+			}
+		}
+	}
+	
+	// 縮小
+	let scale = 1;
+	let x = 0;
+	function minimize(toX) {
+		if (scale < 0.05) {
+			canvasElement.style.display = 'none';
+			return;
+		}
+		ctx.clearRect(0 - x * 2, 0, ctx.canvas.width * (2 - scale), ctx.canvas.height * (2 - scale));
+		ctx.globalAlpha = scale;
+		drawElements(ctx);
+		
+		x = toX * (1 - scale);
+		ctx.setTransform (scale, 0, 0, scale, x, 0);
+		scale *= 0.8;
+		window.requestAnimationFrame(minimize);
+	}
+	// 放大
+	
+	
+	this.minimize = function(toX) {
+		canvasElement.style.display = 'block';
+		minimize(toX);
 	}
 }
